3c947b496b87745ae16771095cb144e9310bbe1b
Ivan Porollo <iporollo@gmail.com>
2022-06-06 17:51:04 -0700

    edit dashboard style

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -80,19 +80,6 @@ const Cards = () => (
   <div>
     <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
       <div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-        <div className="border-b-2 pb-4">
-          <dt className="text-sm font-medium text-gray-500 truncate">
-            Business Status
-          </dt>
-          <dd className="mt-1 text-3xl font-semibold text-green-500">
-            Good Standing
-          </dd>
-        </div>
-        <div className="mt-5">
-          <List />
-        </div>
-      </div>
-      <div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
         <dd className="mb-1 text-3xl font-semibold text-gray-900">
           Vocable Inc.
         </dd>
@@ -112,6 +99,19 @@ const Cards = () => (
         >
           See more
         </Link>
+      </div>
+      <div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+        <div className="border-b-2 pb-4">
+          <dt className="text-sm font-medium text-gray-500 truncate">
+            Business Status
+          </dt>
+          <dd className="mt-1 text-3xl font-semibold text-green-500">
+            Good Standing
+          </dd>
+        </div>
+        <div className="mt-5">
+          <List />
+        </div>
       </div>
       <div className="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
         <div className="mb-2">
