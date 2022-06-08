ab0dbb5fdc3ec4f481f0cfee07b120df95e4355c
Ivan Porollo <iporollo@gmail.com>
2022-06-08 14:03:37 -0700

    style company page

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -5,16 +5,184 @@ export default function Company() {
     <main className="flex-1">
       <div className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">Company</h1>
-        </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          {/* Replace with your content */}
-          <div className="py-4">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+          <div className="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Pied Piper Inc.
+            </h3>
+            <div className="mt-3 sm:mt-0 sm:ml-4">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Edit Information
+              </button>
+            </div>
           </div>
-          {/* /End replace */}
+        </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-4">
+          <InfoCards />
+        </div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-4">
+          <BusinessStanding />
         </div>
       </div>
     </main>
   );
 }
+
+const InfoCards = () => (
+  <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
+    <GeneralInfo />
+    <RegisteredAgentInformation />
+    <AccountantInfo />
+    <LegalInfo />
+  </dl>
+);
+
+const GeneralInfo = () => {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          General Information
+        </h3>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Address</dt>
+            <dd className="mt-1 text-sm text-gray-900">
+              2261 MARKET STREET #4162 SAN FRANCISCO CA 94114
+            </dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">
+              Incorporation Date
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900">06/12/2020</dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">EIN</dt>
+            <dd className="mt-1 text-sm text-gray-900">*******</dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">
+              California Tax ID
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900">C-123456</dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">
+              San Francisco Business Account Number
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900">123456</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+const RegisteredAgentInformation = () => {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Registered Agent
+        </h3>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Agent</dt>
+            <dd className="mt-1 text-sm text-gray-900">
+              Corporation Service Company (CSC)
+            </dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Agent Number</dt>
+            <dd className="mt-1 text-sm text-gray-900">12345678</dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Address</dt>
+            <dd className="mt-1 text-sm text-gray-900">
+              251 Little Falls Drive, Wilmington, DE. 19808
+            </dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">File Number</dt>
+            <dd className="mt-1 text-sm text-gray-900">123456</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+const AccountantInfo = () => {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          CPA Information
+        </h3>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Company</dt>
+            <dd className="mt-1 text-sm text-gray-900">ACME Accounting</dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Email</dt>
+            <dd className="mt-1 text-sm text-gray-900">team@acme.co</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+const LegalInfo = () => {
+  return (
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          Legal Information
+        </h3>
+      </div>
+      <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+        <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Company</dt>
+            <dd className="mt-1 text-sm text-gray-900">Acme Legal </dd>
+          </div>
+          <div className="sm:col-span-1">
+            <dt className="text-sm font-medium text-gray-500">Email</dt>
+            <dd className="mt-1 text-sm text-gray-900">team@acme.co</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+const BusinessStanding = () => (
+  <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+    <div className="px-4 py-5 sm:px-6">
+      <h3 className="text-lg leading-6 font-medium text-gray-900">Standings</h3>
+    </div>
+    <div className="border-t border-gray-200 px-4 py-5 sm:px-6">
+      <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+        <div className="sm:col-span-1">
+          <dt className="text-sm font-medium text-gray-500">Company</dt>
+          <dd className="mt-1 text-sm text-gray-900">Acme Legal </dd>
+        </div>
+        <div className="sm:col-span-1">
+          <dt className="text-sm font-medium text-gray-500">Email</dt>
+          <dd className="mt-1 text-sm text-gray-900">team@acme.co</dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+);
